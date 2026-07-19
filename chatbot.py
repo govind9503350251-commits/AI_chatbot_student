58dad3792236185c889ef3a29abd1b50a1cb28f3
govind9503350251-commits <govind9503350251@gmail.com>
2026-07-19 23:16:13 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,22 @@
+from ollama import chat
 
+while True:
+
+    user = input("You: ")
+
+    if user=="exit":
+        break
+
+    response = chat(
+
+        model="llama3.2",
+
+        messages=[
+
+        {"role":"user","content":user}
+
+        ]
+
+    )
+
+    print(response["message"]["content"])
